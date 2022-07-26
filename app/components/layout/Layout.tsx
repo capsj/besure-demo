4beb4be972c945085f6cca920fab5be3f15c592e
Jeronimo Carlos <jeronimo.carlos@ing.austral.edu.ar>
2022-07-26 18:27:46 +0200

    Set up typescript and some components

---
+++ app/components/layout/Layout.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from 'react';
+
+const Layout: FunctionComponent = ({ children }: any) => (
+  <div className="layout">
+    { children }
+  </div>
+);
+
+export default Layout;
---
+++ pages/quote/index.tsx
@@ -0,0 +1,8 @@
+const Quote = () => {
+    return (
+        <iframe src="https://amedex.web.app/widget?config=GBzUWqXQgdZnGuZNFGqwlcghiNj1" frameBorder="0" width="428"
+                height="900"></iframe>
+    );
+}
+
+export default Quote;
