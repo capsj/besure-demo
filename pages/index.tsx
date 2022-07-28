1edc91c18b8fc70d1e82ed0614d62dbf847ac722
Jeronimo Carlos <jeronimo.carlos@ing.austral.edu.ar>
2022-07-28 17:12:09 +0200

    Display sizing details

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home = () => {
   return (
     <div className={styles.container}>
         <main className="leading-normal tracking-normal text-white">
-            <div className="sm:p-10 sm:pt-24">
+            <div className="sm:p-10">
                 <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left my-6 py-4 px-8 sm:my-0 sm:py-0 sm:px-0">
                         <h1 className="my-4 text-5xl font-bold leading-tight">
                             Your key to a secure future
--- pages/quote/index.tsx
+++ pages/quote/index.tsx
@@ -1,14 +1,23 @@
 import styles from "../../styles/Home.module.css";
+import React, { useState, useEffect } from "react";
 
 const quoteContainer = 'flex flex-col items-center sm:pt-8 gradient-background';
+const iframeSrc = 'https://amedex.web.app/widget?config=GBzUWqXQgdZnGuZNFGqwlcghiNj1';
 
 const Quote = () => {
     return (
         <div className={styles.container}>
             <div className={quoteContainer}>
                 <iframe
-                    src="https://amedex.web.app/widget?config=GBzUWqXQgdZnGuZNFGqwlcghiNj1&isMobile=true"
-                    className='max-w-full rounded-2xl'
+                    src={iframeSrc}
+                    className='max-w-full rounded-2xl hidden md:block'
+                    frameBorder="0"
+                    width="428"
+                    height="900"></iframe>
+
+                <iframe
+                    src={iframeSrc + '&isMobile=true'}
+                    className='max-w-full rounded-2xl block md:hidden'
                     frameBorder="0"
                     width="428"
                     height="900"></iframe>
